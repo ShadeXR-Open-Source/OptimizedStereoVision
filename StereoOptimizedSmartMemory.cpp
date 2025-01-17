224057c39f5bb40d394ace4c8773a4168cfe306b
ahpinder <42349096+ahpinder@users.noreply.github.com>
2025-01-17 13:22:51 -0500

    vectorized argmin calculation

--- StereoOptimizedSmartMemory.cpp
+++ StereoOptimizedSmartMemory.cpp
@@ -138,16 +138,32 @@ int optimizedBlockMatchScalar(cv::Mat &leftMat, cv::Mat &rightMat, cv::Mat &outp
     return 0;
 }
 #ifdef AVX2_ENABLED
+/*
+* Stereo block matching algorithm, optimized with SIMD vector operations
+* @param leftMat OpenCV Mat of left input image
+* @param rightMat OpenCV Mat of right input image
+* @param outputMat OpenCV Mat of output image container
+* @param blockSize width / height of block for block matching, should be odd
+* @param numDisparities number of disparities to check, should be a multiple of 32
+* @return 0 if successful, -1 if input images don't match size, -2 if numDisparities is not a multiple of 32
+*/
 int optimizedBlockMatchAVX2(cv::Mat& leftMat, cv::Mat& rightMat, cv::Mat& outputMat, int blockSize, int numDisparities) {
-    /*
-    * Stereo block matching algorithm, optimized with SIMD vector operations
-    */
+
     if (leftMat.rows != rightMat.rows || leftMat.cols != rightMat.cols) {
         // this algorithm assumes both images are of the same size
         return -1;
     }
+    if (numDisparities % 32 != 0) {
+        // AVX2 implementation uses 32-pixel operations
+        // and I'm too lazy (right now) to deal with edge cases
+        // of partial vectors
+        return -2;
+    }
+    // define local array pointers
     uchar* left = leftMat.isContinuous() ? leftMat.data : leftMat.clone().data;
     uchar* right = leftMat.isContinuous() ? rightMat.data : rightMat.clone().data;
+
+    // size helpers for array indexing + looping
     int blockRad = (blockSize - 1) / 2;
     int numNonzeroDisparities = numDisparities - 1;
     int inWidth = leftMat.cols;
@@ -157,9 +173,14 @@ int optimizedBlockMatchAVX2(cv::Mat& leftMat, cv::Mat& rightMat, cv::Mat& output
     int diffHeight = inWidth - numNonzeroDisparities;
     int xnHeight = diffHeight - 2 * blockRad; // height of x1 and x2, also equals outWidth
     int x1SubLength = xnHeight * numDisparities;
+
+    // argmin helper array
+    uint32_t vecmin[8];
+
+    // output array in Mat container
     outputMat = cv::Mat(outHeight, outWidth, CV_16UC1);
+    // output array pointer
     uint16_t* output = (uint16_t*)outputMat.data;
-    __m256i accumulator, newdata; // general purpose AVX2 registers for SIMD acceleration
 
     // I'm using calloc teehee (hope I don't memleak)
     // diff stores the absolute differences between compared pixels
@@ -214,9 +235,10 @@ int optimizedBlockMatchAVX2(cv::Mat& leftMat, cv::Mat& rightMat, cv::Mat& output
         // first, create outputs
         // run argmin across rows of x2
         uint32_t minval;
+
         uint16_t minind;
         for (int i = 0; i < outWidth; i++) {
-            minval = x2[i * numDisparities];
+            /*minval = x2[i * numDisparities];
             minind = 0;
             for (int j = 1; j < numDisparities; j++) {
                 if (x2[i * numDisparities + j] < minval) {
@@ -224,8 +246,50 @@ int optimizedBlockMatchAVX2(cv::Mat& leftMat, cv::Mat& rightMat, cv::Mat& output
                     minind = j;
                 }
             }
+            output[y * outWidth + i] = minind;*/
+            // set the minimum to the max uint32 value (-1 represented as unsigned)
+            __m256i currentmin = _mm256_set1_epi32(-1);
+            __m256i currentval;
+            __m256i minmask;
+            // first loop around, just find the min value
+            for (int j = 0; j < numDisparities; j += 8) {
+                // load in the next vector (8 values)
+                currentval = _mm256_loadu_si256((const __m256i*)(x2 + i * numDisparities + j));
+                currentmin = _mm256_min_epu32(currentval, currentmin);
+            }
+            // store minimum si256 vector to vecmin so we can access it
+            _mm256_storeu_si256((__m256i*)vecmin, currentmin);
+            // unrolled minimum loop
+            minval = vecmin[0];
+            if (vecmin[1] < minval) minval = vecmin[1];
+            if (vecmin[2] < minval) minval = vecmin[2];
+            if (vecmin[3] < minval) minval = vecmin[3];
+            if (vecmin[4] < minval) minval = vecmin[4];
+            if (vecmin[5] < minval) minval = vecmin[5];
+            if (vecmin[6] < minval) minval = vecmin[6];
+            if (vecmin[7] < minval) minval = vecmin[7];
+            // load minimum back into min vector
+            currentmin = _mm256_set1_epi32(minval);
+            // loop until we find a match for the min value in the vector
+            for (int j = 0; j < numDisparities; j += 8) {
+                currentval = _mm256_loadu_si256((const __m256i*)(x2 + i * numDisparities + j));
+                minmask = _mm256_cmpeq_epi32(currentmin, currentval);
+                if (!_mm256_testz_si256(minmask, minmask)) {
+                    minind = j;
+                    break;
+                }
+            }
+            // now loop within the vector to find the exact value
+            for (int j = 0; j < 8; j++) {
+                if (x2[i * numDisparities + minind + j] == minval) {
+                    minind += j;
+                    break;
+                }
+            }
+            // finally, write out the disparity value
             output[y * outWidth + i] = minind;
         }
+
         // now, exit loop if we are done
         if (y == outHeight - 1) {
             break;
@@ -280,12 +344,18 @@ int main()
     cv::Mat left = cv::imread("C:/Users/ahpin/Documents/opencv_utilities/left.jpg",cv::IMREAD_GRAYSCALE);
     cv::Mat right = cv::imread("C:/Users/ahpin/Documents/opencv_utilities/right.jpg", cv::IMREAD_GRAYSCALE);
     cv::Mat disparity, disparity_uint8;
+    cv::Mat disparity_avx2;
     std::chrono::system_clock::time_point before = std::chrono::system_clock::now();
-    optimizedBlockMatchAVX2(left, right, disparity, 15, 96);
+    optimizedBlockMatchScalar(left, right, disparity, 15, 96);
     std::chrono::system_clock::time_point after = std::chrono::system_clock::now();
     std::chrono::microseconds elapsed_us = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
-    std::cout << "stereo took " << elapsed_us.count() << " microseconds.\n";
-    disparity.convertTo(disparity_uint8, CV_8U);
+    std::cout << "stereo scalar took " << elapsed_us.count() << " microseconds.\n";
+    before = std::chrono::system_clock::now();
+    optimizedBlockMatchAVX2(left, right, disparity_avx2, 15, 96);
+    after = std::chrono::system_clock::now();
+    elapsed_us = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
+    std::cout << "stereo AVX2 took " << elapsed_us.count() << " microseconds.\n";
+    disparity_avx2.convertTo(disparity_uint8, CV_8U);
     cv::imshow("left", left);
     cv::imshow("right", right);
     cv::imshow("disparity", disparity_uint8);
